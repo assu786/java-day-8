4c5293428b9dd023c8044cb2cd7df281b15e3a19
assu786 <shaikaqsa.78@gmail.com>
2024-12-09 14:53:32 +0530

    Add files via upload

---
+++ 2darray.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class D
+{
+  public static void main (String args[])
+  {
+    //intialization of 2D-array
+    int a[][]={{1,2},{3,4}};
+    System.out.println(Arrays.toString(a[0]));
+    System.out.println(Arrays.toString(a[1]));
+  }
+}
